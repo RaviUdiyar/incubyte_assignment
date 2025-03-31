c01d851fb0489e71504098979a31990d049baded
ravisiddarth <ravi-udiyar@x-b-e.com>
2025-03-31 13:51:35 +0530

    feat: spike string calculator class

---
+++ lib/string_calculator.rb
@@ -0,0 +1,13 @@
+# frozen string literal: true
+
+# Input: a string of comma-separated numbers
+# Output: an integer, sum of the numbers
+# numbers: string input. example: "1,2,3" or "1\n2,3"
+class StringCalculator
+  def initialize(numbers)
+    @numbers = numbers
+  end
+
+  def add
+  end
+end
