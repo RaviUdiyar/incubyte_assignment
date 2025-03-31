a1432931af87aea6f0efea5a11724165e25cd356
ravisiddarth <ravi-udiyar@x-b-e.com>
2025-03-31 14:39:42 +0530

    optimize: skip custom delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,7 +4,7 @@
 # Output: an integer, sum of the numbers
 # numbers: string input. example: "1,2,3" or "1\n2,3"
 class StringCalculator
-  DELIMETERS = [',', '\n'].freeze
+  DEFAULT_DELIMETERS = [',', '\n'].freeze
 
   def initialize
     @numbers = ''
@@ -19,9 +19,18 @@ class StringCalculator
     i = 0
     while i < @numbers.size
       char = @numbers[i]
-      result += char.to_i unless DELIMETERS.include?(char)
+      result += char.to_i unless delimiters.include?(char)
       i += 1
     end
     result
+  end
+
+  def delimiters
+    if @numbers.start_with?('//')
+      @numbers = @numbers[2..]
+      [@numbers[0]] + DEFAULT_DELIMETERS
+    else
+      DEFAULT_DELIMETERS
+    end
   end
 end
